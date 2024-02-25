b3968f2f2cd24f4a138ed97a04972a36344f4241
Nikita Ambalov <ambani-tech@outlook.com>
2024-02-25 23:47:23 +0300

    commit after installed react

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { calc } from "./test";
-
-console.log("Hello, World! It's meee.");
-console.log(calc(15, 10));
--- webpack.config.ts
+++ webpack.config.ts
@@ -15,7 +15,7 @@ export default (env: EnvVariables) => {
 
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
-    entry: path.resolve(__dirname, "src", "index.ts"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
     module: {
       rules: [
         {
