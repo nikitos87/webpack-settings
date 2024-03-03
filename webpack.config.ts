61d2c4fd3daa407133b53346289eb373cb5b01d0
Nikita Ambalov <ambani-tech@outlook.com>
2024-03-03 20:21:01 +0300

    commit after added MiniCssExtractPlugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = "production" | "development";
@@ -12,6 +13,7 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
   const isDev = env.mode === "development";
+  const isProd = env.mode === "production";
 
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
@@ -21,7 +23,11 @@ export default (env: EnvVariables) => {
         // порядок имеет значение - ВАЖНО!!!
         {
           test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
         },
         {
           test: /\.tsx?$/,
@@ -50,6 +56,11 @@ export default (env: EnvVariables) => {
         template: path.resolve(__dirname, "public", "index.html"),
       }),
       isDev ? new webpack.ProgressPlugin() : undefined,
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
+        }),
     ].filter(Boolean),
   };
 
